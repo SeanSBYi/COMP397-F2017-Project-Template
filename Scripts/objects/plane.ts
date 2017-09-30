613cf51a0544fcdbe558e9a89b4e4e6acf4a8b2b
Sean (Sangbeom) Yi <isuryun@gmail.com>
2017-09-30 15:18:24 -0400

    Mouse moving

---
+++ Scripts/objects/plane.ts
@@ -0,0 +1,46 @@
+module objects {
+    export class Plane extends createjs.Bitmap {
+        // PRIVATE INSTANCE VARIABLES
+        private width: number;
+        private height: number;
+        private halfWidth: number;
+        private halfHeight: number;
+        
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+        constructor(assetManager:createjs.LoadQueue) {
+            super(assetManager.getResult("plane"));
+
+            this.Start();
+        }
+
+        // PRIVATE METHODS
+        private _checkBounds() {
+            if(this.x >= (640 - this.halfHeight)) {
+                this.x = 640 - this.halfWidth;
+            }
+            if(this.x <= this.halfWidth) {
+                this.x = this.halfWidth;
+            }
+        }
+
+        // PUBLIC METHODS
+        public Start() {
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+            this.halfWidth = this.width * 0.5;
+            this.halfHeight = this.height * 0.5;
+            this.regX = this.halfWidth;
+            this.regY = this.halfHeight;
+            this.x = 320;
+            this.y = 430;
+        }
+
+        public Update() {
+            this.x = this.stage.mouseX;
+            this._checkBounds();
+        }
+    }
+}
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -3,50 +3,34 @@ module scenes {
     // PRIVATE INSTANCE VARIABLES
     private _assetManager:createjs.LoadQueue;
 
-    private _playLabel:objects.Label;
-    private _backButton:objects.Button;
-    private _nextButton:objects.Button;
+    private _plane:objects.Plane;
 
     // PUBLIC PROPERTIES
 
     // CONSTRUCTORS
     constructor(assetManager:createjs.LoadQueue, currentScene:number) {
-      super();
-      this._assetManager = assetManager;
-      this._currentScene = currentScene;
-      this.Start();
+        super();
+        this._assetManager = assetManager;
+        this._currentScene = currentScene;
+        this.Start();
     }
     // PRIVATE METHODS
 
     // PUBLIC METHODS
     public Start():void {
-      this._playLabel = new objects.Label("Game Playing", "40px", "Consolas", "#000000", 320, 240, true);
-      this._backButton = new objects.Button(this._assetManager, "backButton", 100, 340, true);
-      this._nextButton = new objects.Button(this._assetManager, "nextButton", 540, 340, true);
-      this.Main();
+        this._plane = new objects.Plane(this._assetManager);
+        this.Main();
     }
 
     public Update():number {
-      return this._currentScene;
+        this._plane.Update();
+
+        //console.log(this.parent.stage.mouseX);
+        return this._currentScene;
     }
 
     public Main():void {
-
-      this.addChild(this._playLabel);
-
-      this.addChild(this._backButton);
-
-      this.addChild(this._nextButton);
-
-      this._backButton.on("click", () => {
-        this._currentScene = config.START;
-        this.removeAllChildren();
-      });
-
-      this._nextButton.on("click", () => {
-        this._currentScene = config.END;
-        this.removeAllChildren();
-      });
+        this.addChild(this._plane);
     }
   }
 }
